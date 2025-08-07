fba33cde7a5dc8a4059eb150c179b8f2b188ee1e
Md Sanim Mia <154647745+md-sanim-mia@users.noreply.github.com>
2025-08-08 03:17:42 +0600

    Delete dist directory

--- dist/app/constants/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productSearchableFields = void 0;
-exports.productSearchableFields = ['name', 'brand', 'category', 'simptoms'];
